af99d0e1a894c6a7d263bbf8078116694e847210
Aveiro746 <95100461+Aveiro746@users.noreply.github.com>
2025-02-08 13:04:35 -0500

    springboot setup

---
+++ backend/src/main/java/com/growthcompanion/backend/GrowthCompanionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.growthcompanion.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthCompanionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthCompanionBackendApplication.class, args);
+	}
+
+}
